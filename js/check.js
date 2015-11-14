b0633c4a33547af0aff08300fbd01cbaa2cd6df1
Жанна <thankfulfor@ya.ru>
2015-11-14 19:51:46 +0300

    Убрала комментирование и debugger

--- js/check.js
+++ js/check.js
@@ -1,20 +1,19 @@
 function getMessage(a,b) {
 
-	// if (typeof(a) == "boolean") {
-	// 	if (a === true) {
-	// 		return('Я попал в ' + b);
-	// 	}else{
-	// 		return("Я никуда не попал");
-	// 	}
-	// }
+	if (typeof(a) == "boolean") {
+		if (a === true) {
+			return('Я попал в ' + b);
+		}else{
+			return("Я никуда не попал");
+		}
+	}
 
-	// if (typeof(a) == "number") {
-	// 	return('Я прыгнул на ' + a*100 + ' сантиметров');
-	// }
+	if (typeof(a) == "number") {
+		return('Я прыгнул на ' + a*100 + ' сантиметров');
+	}
 
 	if (Array.isArray(a)) {
 
-debugger;
 		if (Array.isArray(b)) {
 			var length = 0;
 			for (var i = 0; i < b.length; i++) {
